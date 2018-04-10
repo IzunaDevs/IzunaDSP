6db7ba652c0f9ca5f69523135e88d70e525d8277
Martmists <martmists@gmail.com>
2018-04-10 21:01:59 +0200

    A lot of progress on this
    Signed-off-by: Martmists <martmists@gmail.com>

---
+++ izunadsp/abc/DSPPart.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+class DSPPart:
+    def handle(self, frame: np.array) -> np.array:
+        raise NotImplementedError
---
+++ izunadsp/core/manager.py
@@ -0,0 +1,30 @@
+from io import BytesIO
+
+from essentia.standard import MonoLoader, FrameGenerator, Windowing
+
+from izunadsp.abc.DSPPart import DSPPart
+
+
+class Manager:
+    def __init__(self):
+        self.parts = []
+
+    def register_part(self, part: DSPPart):
+        self.parts.append(part)
+
+    def passthrough(self, filename: str) -> BytesIO:
+        audio = MonoLoader(filename=filename)()
+
+        w = Windowing(type="hann")
+
+        frames = []
+
+        for frame in FrameGenerator(audio, frameSize=1024, hopSize=512):
+            for part in self.parts:
+                frame = part.handle(w(frame))
+
+            frames.append(frame)
+
+            break
+
+        return frames
