Fix right channel being silent for some reason
Signed-off-by: Martmists <martmists@gmail.com>

izunadsp/abc/dsp_part.py
@@ -23,4 +23,7 @@ class DSPPart:
 
     @staticmethod
     def to_stereo(audio: np.array) -> tuple:
-        return StereoDemuxer()(audio)
+        left, right = StereoDemuxer()(audio)
+        if not [r for r in right if r != 0]:
+            right = left.copy()
+        return left, right

izunadsp/parts/reverb.py
@@ -20,7 +20,7 @@ class Reverb(DSPPart):
             beginning = [0] * offset * (i + 1)
             end = audio_bytes[:-offset * (i + 1)]
             multiplied_end = end * ((1 - self.decay) ** (i + 1))
-            delayed_bytes = delayed_bytes + np.append(beginning, multiplied_end)
+            delayed_bytes += np.append(beginning, multiplied_end)
         return delayed_bytes
 
     def handle(self, audio: np.array) -> np.array: